a154f23c8513d67e2680a35ec67f73ff96ab3c64
andysigg123 <110823661+andysigg123@users.noreply.github.com>
2023-09-22 10:58:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 alert('Olá, mundo')
 function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+    document.querySelector('#som_tecla_pom')();
 }
 document.querySelectorAll('.tecla');
 listaDeTecla[0].onclick = tocaSomPom;
+const listaDeTecla = document.querySelectorAll('.tecla');
+listaDeTecla[0].onclick = tocaSomPom;
+let contador = 0;
+//Estrutura de repetição - Enquanto
+while(contador < listaDeTecla.length) {
+listaDeTecla[contador].onclick = tocaSomPom;
+
+contador = contador + 1;
+}
