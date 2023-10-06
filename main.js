94e03ae8e667666e15bba1cd5435da929426db52
andysigg123 <110823661+andysigg123@users.noreply.github.com>
2023-10-06 10:58:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-alert('Olá, mundo')
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom')();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
-document.querySelectorAll('.tecla');
-listaDeTecla[0].onclick = tocaSomPom;
+
 const listaDeTecla = document.querySelectorAll('.tecla');
+
+//para
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+
+    const tecla =  listaDeTecla[contador];
+const intrumento = tecla.classList[1];
+const idAudio = `Som_${intrumento}`;
+
+tecla.onclick = function (){
+    tocaSom(idAudio);
+}
+
+tecla.onkeydown = function (evento){
+    
+}
+
+document.querySelectorAll('.tecla');
+listaDeTecla[0].onclick = tocaSom;
+
 listaDeTecla[0].onclick = tocaSomPom;
 let contador = 0;
 //Estrutura de repetição - Enquanto
 while(contador < listaDeTecla.length) {
 listaDeTecla[contador].onclick = tocaSomPom;
 
-contador = contador + 1;
+contador = contador + 1; 
+console.log(contador);
 }
